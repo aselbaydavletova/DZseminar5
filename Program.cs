8f6b60148142d7cbd12f09811a52f862a4c7ad89
Asel <a.2019.916bt@yandex.ru>
2023-09-15 22:41:42 +0300

    Домашняя работа по серминару 5

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+// System.Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// int count = 0;
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] % 2 == 0)
+//     count++;
+// }
+
+// System.Console.WriteLine($"Количество четных чисел в массиве -> {count} ");
+
+// void FillArrayRandomNumbers(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(100,1000);
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//     System.Console.Write(array[i] + " ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// int size = 4;
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+// int sumNumbersEvenIndex = 0;
+
+// for (int i = 1; i < numbers.Length; i += 2)
+// {
+//     sumNumbersEvenIndex += numbers[i];
+// }
+// System.Console.WriteLine(sumNumbersEvenIndex);
+
+// void FillArrayRandomNumbers(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//      array[i] = new Random().Next(-100, 101);   
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+// int size = 10;
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+// int max = numbers[0];
+// int min = numbers[0];
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+// if (numbers[i] > max)
+// {
+//     max = numbers[i];
+// }    
+// else if (numbers[i] < min);
+// {
+//     min = numbers[i];
+// }
+// }
+
+// System.Console.WriteLine($"Минимальное число: {min}");
+// System.Console.WriteLine($"Максимальное число: {max}");
+// System.Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
+
+// void FillArrayRandomNumbers(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(1, 555);
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//      System.Console.Write(array[i] + " ");   
+//     }
+//     System.Console.WriteLine();
+// }
